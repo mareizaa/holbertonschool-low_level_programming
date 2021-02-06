4072f05278df3306e49e80413e54a0307f9729eb
Marcela <marce.3210@gmail.com>
2021-02-06 01:20:47 +0000

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+		if (a != 113 && a != 101)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}

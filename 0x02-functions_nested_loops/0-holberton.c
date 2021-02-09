e7484c2639ac321d5bc1d1c4195b3433bad812ec
Marcela <marce.3210@gmail.com>
2021-02-09 15:05:27 +0000

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char hol[10] = "Holberton\n";
+	int pos;
+
+	for (pos = 0; pos <= 10; pos++)
+	{
+		_putchar(hol[pos]);
+	}
+	return (0);
+}

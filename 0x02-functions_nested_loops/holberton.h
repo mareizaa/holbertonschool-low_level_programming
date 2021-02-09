c837d187f31f6be9727ef99064663da9c1e17340
Marcela <marce.3210@gmail.com>
2021-02-09 15:20:07 +0000

    header

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+
+/* _putchar - print */
+int _putchar(char c);
+/* prints the alphabet, in lowercase, followed by a new line */
+void print_alphabet(void);
+/* prints 10 times the alphabet, in lowercase, followed by a new line */
+void print_alphabet_x10(void);
+/* checks for lowercase character */
+int _islower(int c);
+/* checks for alphabetic character */
+int _isalpha(int c);
+/* prints the sign of a number */
+int print_sign(int n);
+/* computes the absolute value of an integer */
+int _abs(int);
+/*prints the last digit of a number */
+int print_last_digit(int);

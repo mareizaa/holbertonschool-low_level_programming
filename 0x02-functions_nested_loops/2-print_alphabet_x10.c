9b1c242f0dc91944551b8ad784776e7a174d0c67
Marcela <marce.3210@gmail.com>
2021-02-09 19:43:08 +0000

    print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int run, abc;
+
+	for (run = 1; run <= 10; run++)
+		{
+			for (abc = 'a'; abc <= 'z';abc++)
+			{
+				_putchar(abc);
+			}
+			_putchar('\n');
+		}
+}
+

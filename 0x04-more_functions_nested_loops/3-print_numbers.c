9b9f41e27f7d8e0d53b68316080aef708a2872d4
Marcela <marce.3210@gmail.com>
2021-02-11 22:34:36 +0000

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - prints the numbers 0 to 9
+ *
+ *Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n >= 9; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}

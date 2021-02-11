ad9343414534905ecb7530faf60961c899faa6d7
Marcela <marce.3210@gmail.com>
2021-02-11 18:29:11 +0000

    0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_isupper - checks for uppercase
+ *
+ *@c: print character
+ *
+ *Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

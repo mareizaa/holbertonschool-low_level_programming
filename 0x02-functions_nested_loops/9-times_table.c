a2b7e4ced7f898dd44c605ca1ee6b5aee97e9aa2
Marcela <marce.3210@gmail.com>
2021-02-11 01:50:15 +0000

    Times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* times_table - prints the 9 times table
+*
+* Return: Always 0.
+*/
+
+void times_table(void)
+{
+	int f, c, r;
+
+	for (f = 0; f <= 9; f++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			r = (f * c);
+			_putchar('0' + r);
+		}		
+		if (r <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + r);
+		}  
+		_putchar('\n');
+	}
+}

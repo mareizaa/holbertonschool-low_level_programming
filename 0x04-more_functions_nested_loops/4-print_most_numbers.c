8eaec13fe6ae0ed86907276949997d8e9a00c5da
Marcela <marce.3210@gmail.com>
2021-02-11 23:04:35 +0000

    not 2_4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers - prints the numbers 0 to 9
+ *
+ *Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		if (c != '2' && c != '4')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

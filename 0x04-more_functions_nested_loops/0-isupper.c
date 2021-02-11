3630803e4e156e687be21b014771e194431f5855
Marcela <marce.3210@gmail.com>
2021-02-11 18:09:21 +0000

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_isupper - checks for uppercase
+ *
+ *@c: print character
+ *
+ *Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

3b3912404fd434aa7dbcdd0d0a63e6ea65453392
Marcela <marce.3210@gmail.com>
2021-02-12 01:11:48 +0000

    act

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -12,3 +12,9 @@ void print_numbers(void);
 void print_most_numbers(void);
 /* prints 10 times the numbers, from 0 to 14 */
 void more_numbers(void);
+/* draws a straight line in the terminal */
+void print_line(int n);
+/* draws a diagonal line on the terminal */
+void print_diagonal(int n);
+/* prints a square, followed by a new line */
+void print_square(int size);

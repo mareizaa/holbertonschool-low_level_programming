a92774afbc8e950c0dfd40c784d95481f4a51605
Marcela <marce.3210@gmail.com>
2021-02-16 22:28:23 +0000

    Library

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,22 @@
+/* _putchar - print */
+int  _putchar(char c);
+/* updates the value it points to to 98 */
+void reset_to_98(int *n);
+/* swaps the values of two integers */
+void swap_int(int *a, int *b);
+/* returns the length of a string */
+int _strlen(char *s);
+/* prints a string */
+void _puts(char *str);
+/* prints a string, in reverse */
+void print_rev(char *s);
+/* reverses a string */
+void rev_string(char *s);
+/* prints every other character of a string */
+void puts2(char *str);
+/* prints half of a string */
+void puts_half(char *str);
+/* prints n elements of an array of integers */
+void print_array(int *a, int n);
+/* copies the string pointed to by src */
+char *_strcpy(char *dest, char *src);

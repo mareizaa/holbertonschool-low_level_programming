d769dbf3a57d6718adc2bce8b76e46abdcd118e1
Marcela <marce.3210@gmail.com>
2021-02-18 22:14:01 +0000

    Header

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,20 @@
+/* funtion putchar */
+int _putchar(char c);
+/* concatenates two strings */
+char *_strcat(char *dest, char *src);
+/* concatenates two strings */
+char *_strncat(char *dest, char *src, int n);
+/* copies a string */
+char *_strncpy(char *dest, char *src, int n);
+/* compares two strings */
+int _strcmp(char *s1, char *s2);
+/* reverses the content of an array of integers */
+void reverse_array(int *a, int n);
+/* changes all lowercase letters of a string to uppercase */
+char *string_toupper(char *);
+/* capitalizes all words of a string */
+char *cap_string(char *);
+/* encodes a string into 1337 */
+char *leet(char *);
+/* encodes a string using rot13 */
+char *rot13(char *);

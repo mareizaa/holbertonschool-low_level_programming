b3606d15ac043ba183f78b093616cfded0bac5da
Marcela <marce.3210@gmail.com>
2021-02-20 23:40:51 +0000

    Concatenar

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: string detino
+ *@src: string two
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l, a;
+
+	for (l = 0; dest[l] != '\0'; l++)
+	{
+	}
+	for (a = 0; src[a] != '\0'; a++, l++)
+	{
+		dest[l] = src[a];
+	}
+	dest[l] = '\0';
+	return (dest);
+}

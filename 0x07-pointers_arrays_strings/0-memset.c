9705a487972393b00a83752c690dd74441777684
Marcela <marce.3210@gmail.com>
2021-02-23 00:04:35 +0000

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _menset - Fills memory with a constant byte
+ * @s: String
+ * @b: constant byte
+ * @n: lengh
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int l;
+
+	for (l = 0; l < n; l++)
+		s[l] = b;
+	return (s);
+}

0fd009d52cd82d8f46fd2c5c7836b2ae4665d964
Marcela <marce.3210@gmail.com>
2021-02-23 04:06:57 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *_strchr - locates a character in a string
+ *@s: String
+ *@c: occurrence
+ *Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+		if (s[l] == c)
+		{
+			return (s + l);
+		}
+		if (!*s)
+		{
+			return ('\0');
+		}
+	}
+	return ('\0');
+}

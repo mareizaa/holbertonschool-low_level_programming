76bd29e76e80715ddf81fe439548d73898acbcbb
mareizaa <marce.3210@gmail.com>
2021-03-02 17:10:45 -0500

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int r;
 
-	p = (char*)malloc(size*sizeof(char));
+	p = (char *)malloc(size * sizeof(char));
 	if (p == '\0')
 	{
 		return ('\0');

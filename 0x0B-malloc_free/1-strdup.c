2255ca6e430af86e4afaa099396d01cd52b49923
mareizaa <marce.3210@gmail.com>
2021-03-02 20:28:28 -0500

    str

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,24 +13,24 @@ char *_strdup(char *str)
 	char *p;
 	int l, r;
 
+	if (str == '\0')
+	{
+		return ('\0');
+	}
 	for (l = 0; str[l] != '\0'; l++)
 	{
 	}
 
 	p = malloc((l + 1) * sizeof(char));
-
-	for (r = 0; r < l; r++)
-	{
-		p[r] = str[r];
-	}
-	if (str == '\0')
-	{
-		return ('\0');
-	}
 	if (p == '\0')
 	{
 		return ('\0');
 	}
+	for (r = 0; str[r] != '\0'; r++)
+	{
+		p[r] = str[r];
+	}
+
 	return (p);
 	free(p);
 }

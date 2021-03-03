621332aa404da884233a64ca9ad9a75aa651267e
mareizaa <marce.3210@gmail.com>
2021-03-02 19:08:34 -0500

    str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - array type char
+ * @str: - pointer
+ * Return: pointer.
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int l, r;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+
+	p = (char *)malloc(l * sizeof(char));
+
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+
+	for (r = 0; r < l; r++)
+	{
+		p[r] = str[r];
+	}
+	return (p);
+}

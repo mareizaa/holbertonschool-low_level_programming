d498d9a8d232bbacd6ee888bc666ea66f76580df
mareizaa <marce.3210@gmail.com>
2021-03-06 20:00:54 -0500

    puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *puts_half - print string
+ *
+ *@str: print character
+ *
+ *Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int l, n, par;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+	if ((l % 2) == 0)
+	{
+		n = (l / 2);
+		for (par = n; par < l; par++)
+		{
+			_putchar(str[par]);
+		}
+	}
+
+	else
+	{
+		n = (l - 1) / 2;
+		for (par = n; par < l; par++)
+		{
+			_putchar(str[par]);
+		}
+	}
+	_putchar('\n');
+}

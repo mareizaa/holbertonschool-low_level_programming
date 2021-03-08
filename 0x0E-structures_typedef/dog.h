e2e6e91ce2812af954a089bccea0f1b95f3be586
mareizaa <marce.3210@gmail.com>
2021-03-08 15:48:44 -0500

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * @name: nombre
+ * @age: age
+ * @owner: owner
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+#endif

37d1b48e341d00a17c1b8dbbd38ba7562a0adbe1
mareizaa <marce.3210@gmail.com>
2021-03-07 19:13:16 -0500

    string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *cap_string - changes all lowercase letters
+ *@str: string
+ *
+ *Return: Always 0.
+ */
+char *cap_string(char *str)
+{
+	int l, l2;
+	char esp[] = " \t\n,;.!?\"(){}";
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+		for (l2 = 0; esp[l2] != '\0'; l2++)
+		{
+			if (str[l] == esp[l2])
+			{
+				if (str[l + 1] >= 97 && str[l + 1] <= 122)
+				{
+					str[l + 1] = str[l + 1] - 32;
+				}
+			}
+		}
+	}
+	return (str);
+}

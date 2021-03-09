00f1f9d5c058ca85b3f385c026feb9a84210e6bc
mareizaa <marce.3210@gmail.com>
2021-03-08 19:38:31 -0500

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,5 @@ char *name;
 float age;
 char *owner;
 } dog;
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

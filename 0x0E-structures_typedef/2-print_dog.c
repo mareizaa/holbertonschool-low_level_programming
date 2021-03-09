9823e1207351a7a4be643b1e517e9b1b5dc9fed8
mareizaa <marce.3210@gmail.com>
2021-03-08 21:33:44 -0500

    Change

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,19 +11,28 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if ((*d).name == '\0')
+	if ((*d).name == NULL)
 	{
 		printf("Name: (nil)\n");
 	}
-	printf("Name: %s\n", (*d).name);
+	else
+	{
+		printf("Name: %s\n", (*d).name);
+	}
 	if ((*d).age < 0)
 	{
 		printf("Age: (nil)\n");
 	}
-	printf("Age: %f", (*d).age);
-	if ((*d).owner == '\0')
+	else
+	{
+		printf("Age: %f", (*d).age);
+	}
+	if ((*d).owner == NULL)
 	{
 		printf("Owner: (nil)\n");
 	}
-	printf("Owner: %s\n", (*d).owner);
+	else
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
 }

1347529fb08b9404778122165e6f6fa26aa81f5c
mareizaa <marce.3210@gmail.com>
2021-03-09 19:03:25 -0500

    new

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - copy struct
+ * @name: nombre
+ * @age: age
+ * @owner: owner
+ * Return: Always 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	int cn, co, n, o;
+
+	if (name == '\0' || owner == '\0')
+		return ('\0');
+	p = malloc(sizeof(dog_t));
+	if (p == '\0')
+		return ('\0');
+	for (cn = 0; name[cn] != '\0'; cn++)
+	{}
+	for (co = 0; owner[co] != '\0'; co++)
+	{}
+	(*p).name = malloc((cn + 1) * (sizeof(char)));
+	if ((*p).name == '\0')
+	{
+		free(p);
+		return ('\0');
+	}
+	(*p).owner = malloc((co + 1) * (sizeof(char)));
+	if ((*p).owner == '\0')
+	{
+		free((*p).name);
+		free(p);
+		return ('\0');
+	}
+	for (n = 0; n < cn; n++)
+		(*p).name[n] = name[n];
+	(*p).age = age;
+	for (o = 0; o < co; o++)
+		(*p).owner[o] = owner[o];
+	return (p);
+}

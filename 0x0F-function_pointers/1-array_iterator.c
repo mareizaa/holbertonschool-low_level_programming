f1bee6d5b62e908e99a72dade18d1e2f11040f12
mareizaa <marce.3210@gmail.com>
2021-03-10 19:39:59 -0500

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long c;
 
-	if (action != NULL)
+	if (action != NULL && array != NULL)
 	{
 		for (c = 0; c < size; c++)
 		{

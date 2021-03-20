675727b1c3440ccb17d0c2bcee3d5b8d8f6f6f51
mareizaa <marce.3210@gmail.com>
2021-03-19 23:07:48 -0500

    list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free struct
+ * @head: Pointer struct
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *aux;
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux->str);
+		free(aux);
+	}
+}

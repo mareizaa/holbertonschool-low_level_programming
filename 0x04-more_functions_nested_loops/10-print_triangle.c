acb43b0047f0bbaf7f2084a5a97f084c74a4d0a7
mareizaa <marce.3210@gmail.com>
2021-03-20 20:58:38 -0500

    Print Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *print_triangle - draws a triangle
+ *@size: size
+ *Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	int espaci, num, aux;
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	for (aux = 0; aux < size; aux++)
+	{
+  		for (espaci = size - 1 - aux; espaci > 0; espaci--)
+  		{
+    		_putchar(' ');
+  		}
+  		for (num = aux + 1; num > 0; num--)
+		{	
+			_putchar('#');
+		}	
+	_putchar('\n');
+	}
+}

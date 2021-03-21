8409499da6e03dd7948e6e9f1f9df53fe383947b
mareizaa <marce.3210@gmail.com>
2021-03-20 21:04:02 -0500

    Print Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,20 +9,21 @@
 void print_triangle(int size)
 {
 	int espaci, num, aux;
-	if (size == 0)
+
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	for (aux = 0; aux < size; aux++)
 	{
-  		for (espaci = size - 1 - aux; espaci > 0; espaci--)
-  		{
-    		_putchar(' ');
-  		}
-  		for (num = aux + 1; num > 0; num--)
-		{	
+		for (espaci = size - 1 - aux; espaci > 0; espaci--)
+		{
+			_putchar(' ');
+		}
+		for (num = aux + 1; num > 0; num--)
+		{
 			_putchar('#');
-		}	
+		}
 	_putchar('\n');
 	}
 }

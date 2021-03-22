4dfb2a548538ac82130a553471d17e79b86fa868
mareizaa <marce.3210@gmail.com>
2021-03-21 21:57:52 -0500

    firts

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main_c(void)__attribute__ ((constructor));
+/**
+ * main_c - print.
+ *
+ * Return: Always 0.
+ */
+int main_c(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}

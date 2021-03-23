07c0609ee6072aa7e00499cd3cac19b9ff3f6143
mareizaa <marce.3210@gmail.com>
2021-03-23 10:50:49 -0500

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_listint - prints all the elements of a listint_t list.
  * @h: Pointer struct
  * Return: Lenght h.
  */

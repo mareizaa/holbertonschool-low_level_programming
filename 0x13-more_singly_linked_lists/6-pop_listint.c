81d5e78c211d501e8952c590d454945d0aef5cce
mareizaa <marce.3210@gmail.com>
2021-03-23 21:40:17 -0500

    delete node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,15 +8,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *aux;
+	int num;
 
 	while (head != NULL && *head != NULL)
 	{
 		aux = (*head)->next;
-		aux->n = (*head)->n;
+		num = (*head)->n;
 		free(*head);
 		*head = aux;
 
-		return (aux->n);
+		return (num);
 	}
 	return (0);
 }

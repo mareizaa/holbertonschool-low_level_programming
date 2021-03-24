790a73339fde3e777abba8b6303d2a44ae3a3ca3
mareizaa <marce.3210@gmail.com>
2021-03-23 22:43:14 -0500

    index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Node index
+ * @head: Double Pointer struct
+ * Return: void.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *aux;
+	unsigned int num;
+
+	if (head != NULL)
+	{
+		aux = head;
+		for (num = 0; aux->next != NULL; num++)
+		{
+			aux = head->next;
+			if (num == index)
+			{
+				return (aux->next);
+			}
+		}
+		if (num != index)
+		{
+			return (NULL);
+		}
+	}
+	return (NULL);
+}

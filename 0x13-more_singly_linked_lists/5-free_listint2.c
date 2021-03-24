e0bb4e7e62c928b1a479b33ae410aa54ebb622e6
mareizaa <marce.3210@gmail.com>
2021-03-23 20:39:04 -0500

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free struct
+ * @head: Double Pointer struct
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	while (head != NULL && *head != NULL)
+	{
+		aux = *head;
+		*head = (*head)->next;
+		free(aux);
+	}
+	*head = NULL;
+}

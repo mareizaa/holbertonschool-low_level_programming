efe8730bbc6b08324901934d2868e8ca7514e381
mareizaa <marce.3210@gmail.com>
2021-03-25 21:11:23 -0500

    print

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: decimal.
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 1)
+	{ 
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1)? '1' : '0');
+}

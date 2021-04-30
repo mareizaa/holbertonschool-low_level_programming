63b3fa43aaadcaca600fdc2cae1013678c6f27e5
mareizaa <marce.3210@gmail.com>
2021-04-30 13:51:19 -0500

    print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: Pointer struct
+ * Return: Lenght h.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int len;
+
+	for (len = 0; h != NULL; len++)
+	{
+			printf("%i\n", h->n);
+			h = h->next;
+	}
+	return (len);
+}

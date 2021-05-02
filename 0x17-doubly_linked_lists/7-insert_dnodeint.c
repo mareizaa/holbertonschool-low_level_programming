f7cc5dcf3bcd8ce8450098b682b04240483120e3
mareizaa <marce.3210@gmail.com>
2021-05-02 18:08:17 -0500

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *aux = *h, *newnode, *tmp;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (!h)
 		return (NULL);

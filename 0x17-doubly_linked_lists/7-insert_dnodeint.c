429e699fc72a91e0a9a49a40d4e7d56c2cb1c10f
mareizaa <marce.3210@gmail.com>
2021-05-09 22:28:43 -0500

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,26 +9,32 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux = *h, *newnode, *tmp;
-	unsigned int i = 0;
+	dlistint_t *aux = *h, *newnode, *tmp = *h;
+	unsigned int i = 0, len;
 
 	if (!h)
 		return (NULL);
+	for (len = 0; tmp != NULL; len++)
+		tmp = tmp->next;
+	if (idx > len - 1)
+		return (NULL);	
 	newnode = malloc(sizeof(dlistint_t));
 	if (!newnode)
 		return (NULL);
+	newnode->n = n;
+	if (idx == len)
+	{	newnode->next = NULL;
+		newnode->prev = tmp;
+		tmp->next = newnode;
+	}
 	if (*h == NULL)
-	{
-		newnode->n = n;
-		newnode->next = NULL;
+	{	newnode->next = NULL;
 		newnode->prev = NULL;
 		*h = newnode;
 		return (newnode);
 	}
 	if (idx == 0)
-	{
-		newnode->n = n;
-		newnode->next = aux;
+	{	newnode->next = aux;
 		newnode->prev = NULL;
 		*h = newnode;
 		return (newnode);
@@ -39,11 +45,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		aux = aux->next;
 	}
-	newnode->n = n;
 	newnode->prev = aux->prev;
 	newnode->next = aux;
 	aux->prev = newnode;
-	tmp = newnode->prev;
-	tmp->next = newnode;
+	newnode->prev->next = newnode;
 	return (newnode);
 }

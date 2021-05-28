590ea60c13efc575b41a93192967ff7453162fa6
mareizaa <marce.3210@gmail.com>
2021-05-27 20:03:12 -0500

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size hash table
+ * Return: hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table_new = NULL;
+
+	table_new = malloc(sizeof(hash_table_t));
+	if (!table_new)
+		return (NULL);
+
+	table_new->size = size;
+
+	table_new->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table_new->array)
+		return (NULL);
+
+	return (table_new);
+}

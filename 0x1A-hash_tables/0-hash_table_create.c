0285230d93bc0575d996e8ba6648d3fd890832e0
mareizaa <marce.3210@gmail.com>
2021-05-28 12:10:12 -0500

    size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table_new = NULL;
+	unsigned long int i;
 
 	table_new = malloc(sizeof(hash_table_t));
 	if (!table_new)
 		return (NULL);
 
+	table_new->size = size;
+
 	table_new->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table_new->array)
 		return (NULL);
 
+	for (i = 0; i < size; i++)
+	{
+		table_new->array[i] = NULL;
+	}
 	return (table_new);
 }

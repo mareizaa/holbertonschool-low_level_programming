a2bf3ed249ca8257e40f5ce2df71981d795d4afd
mareizaa <marce.3210@gmail.com>
2021-05-28 18:33:38 -0500

    key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,10 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (new_node == NULL)
 			return (0);
 
-		if (!strcmp(key, ""))
-		{
-			new_node->key = strdup(key);
-		}
+		new_node->key = strdup(key);
 		new_node->value = strdup(value);
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
@@ -33,13 +30,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node = malloc(sizeof(hash_node_t));
 		if (new_node == NULL)
 			return (0);
-		
-		if (!strcmp(key, ""))
-		{
-			new_node->key = strdup(key);
-		}
 
 		new_node->value = strdup(value);
+		new_node->key = strdup(key);
 		new_node->next = NULL;
 		ht->array[index] = new_node;
 		return (1);

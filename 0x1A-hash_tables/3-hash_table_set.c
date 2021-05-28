369c31c0786b95015115d62e3447840990e8aaa2
mareizaa <marce.3210@gmail.com>
2021-05-28 17:41:36 -0500

    hash

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - creates a hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: 1 successful
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int id = hash_djb2((const unsigned char *)key);
+	unsigned long int index = id % ht->size;
+	hash_node_t *new_node;
+
+	if (!key || *key == '\0')
+	{
+		return (0);
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->value = strdup(value);
+	new_node->key = strdup(key);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}

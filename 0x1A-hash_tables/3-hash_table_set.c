136485ab847bbbb5e8e44f0d8aa683e40ca2d5be
mareizaa <marce.3210@gmail.com>
2021-05-28 18:24:44 -0500

    key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,12 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = id % ht->size;
 	hash_node_t *new_node;
 
-	if (strcmp(key, ""))
-	{
-		return (0);
-	}
-
-
 	if (ht->array[index] != NULL)
 	{
 		new_node = malloc(sizeof(hash_node_t));

1114a6f9caea513da3f604e3c5c9a8b6ca7028e7
mareizaa <marce.3210@gmail.com>
2021-05-28 18:55:52 -0500

    hash_tables.h

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ * Return: Value associated
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int id = hash_djb2((const unsigned char *)key);
+	unsigned long int index = id % ht->size;
+	hash_node_t *new_node = ht->array[index];
+
+	while (new_node != NULL)
+	{
+		if (new_node->key == key)
+			return (new_node->value);
+		new_node = new_node->next;
+	}
+	return (NULL);
+}

c075f67ade57f7a4a872a32b5cb22b541aa47d4f
mareizaa <marce.3210@gmail.com>
2021-05-28 18:26:00 -0500

    key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,6 +9,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	
+	if (strcmp(key, ""))
+	{
+		return (0);
+	}
+
 	unsigned long int id = hash_djb2((const unsigned char *)key);
 	unsigned long int index = id % ht->size;
 	hash_node_t *new_node;

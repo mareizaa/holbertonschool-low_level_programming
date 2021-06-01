43769b6c6278ade2f6c39132deeb2d46f5f97c9f
mareizaa <marce.3210@gmail.com>
2021-05-31 20:35:50 -0500

    hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,10 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *aux = NULL;
 
 	if (ht == NULL || ht->array == NULL)
-	{
 		return;
-	}
-	
 
 	printf("{");
 	for (; index < ht->size; index++)

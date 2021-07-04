952aa269c3be1a7f99b5157b69b787a8c01be206
mareizaa <marce.3210@gmail.com>
2021-07-04 16:32:50 -0500

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    count = 0
+    side = 0
+    height = len(grid)
+
+    for h in range(height):
+        for w in range(len(grid[h])):
+            if grid[h][w] != 0:
+                count += 1
+                if grid[h - 1][w] and h != 0:
+                    side += 1
+                if grid[h][w - 1] and w != 0:
+                    side += 1
+    return (4 * count) - (2 * side)
